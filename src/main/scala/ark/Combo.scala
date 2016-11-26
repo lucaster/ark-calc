0ca44cc2f049bc3ed765c13ab2ce4045f67eedf4
lucaster <lucaster@hotmail.it>
2016-11-26 16:56:09 +0100

    Refact

--- src/main/scala/ark/Combo.scala
+++ src/main/scala/ark/Combo.scala
@@ -1,37 +1,38 @@
 package ark
 
-case class Combo(private val _hits: List[Hit] = Nil) {
+import scala.annotation.tailrec
+
+case class Combo(val hits: List[Hit] = Nil) {
 
   /**
    * Current total ark
    */
-  def ark: Int = {
-    def ark(hits: List[Hit]): BigDecimal = {
-      if (hits.isEmpty) {
-        0
-      } else {
-        val prev = hits.take(hits.size - 1)
-        val last = hits.last
-        def sum(hits: List[Hit]) = hits.map(_.damage).sum * hits.map(_.multiplier).sum
-        if (prev.contains(last)) 2 * ark(prev) else ark(prev) + sum(hits)
-      }
-    }
-    ark(hits).toInt;
-  }
+  def ark: Int = Combo.ark(hits).toInt
 
   /**
    * Combo ark if the hit were added.
    */
-  def ark(candidate: Hit): Int = throw new UnsupportedOperationException("Implement me!");
-
-  def hits: List[Hit] = _hits;
+  def ark(candidate: Hit): Int = Combo.ark(hits :+ candidate).toInt;
 
   /**
    * Appends the hit at the end of the Combo.
    *
    * @return a new Combo instance
    */
-  def append(hit: Hit): Combo = copy(_hits :+ hit)
+  def append(hit: Hit): Combo = copy(hits :+ hit)
+}
+case object Combo {
+  def sum(hits: List[Hit]) = hits.map(_.damage).sum * hits.map(_.multiplier).sum
+
+  def ark(hits: List[Hit]): BigDecimal = {
+    if (hits.isEmpty) {
+      0
+    } else {      
+      val prev = hits.take(hits.size - 1)
+      val last = hits.last
+      if (!prev.contains(last)) ark(prev) + sum(hits) else 2 * ark(prev)
+    }
+  }
 }
 
 case class Hit(val trap: Trap, val bonusMultiplier: BigDecimal = 0.0) {
