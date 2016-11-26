83bd93f1ad729b49f13f9876fbe998f9a668282d
lucaster <lucaster@hotmail.it>
2016-11-26 17:24:06 +0100

    Added spec

---
+++ src/test/scala/ark/SameTrapComboSpec.scala
@@ -0,0 +1,26 @@
+package ark
+
+import org.scalatest.Assertions._
+import org.scalatest.FunSpec
+import ark.Trap._
+
+class SameTrapComboSpec extends FunSpec {
+
+  describe("A n x (Trap) combo") {
+
+    val n = 15;
+
+    val hits = (1 to n).toList.map(_ => new Hit(SpringFloor, 0.1));
+
+    val combo = new Combo(hits);
+
+    it("has n hits") {
+      assert(n === combo.hits.size);
+    }
+
+    it("has ark = ark(hit)*2^(n-1)") {
+      val first = hits.head
+      assert(combo.ark === first.damage * first.multiplier * Math.pow(2, n - 1));
+    }
+  }
+}
